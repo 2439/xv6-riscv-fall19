0e33fdb36bb4491eb77cb2d971f20ad1c510f7de
2439 <45897549+2439@users.noreply.github.com>
2020-12-24 17:14:17 +0800

    util-pingpong.description

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,23 +2,24 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-    int p1[2];
-    int p2[2];
-    char n1[5];
-    char n2[5];
+    int p1[2];      // 主进程->子进程
+    int p2[2];      // 子进程->主进程
+    char n1[5];     // 子进程读
+    char n2[5];     // 主进程读
 
-    if (pipe(p1) == -1 || pipe(p2) == -1) {
+    // 管道创建
+    if (pipe(p1) == -1 || pipe(p2) == -1) {     // p[0] 读，p[1]写
         printf("pipe error\n");
         exit();
     }
+    // 进程创建
     int pid = fork();
     if(pid < 0){
         printf("fork error\n");
         exit();
-    }
-    else if (pid == 0) {
-        close(p1[1]);
-        read(p1[0], n1, sizeof(n1));
+    } else if (pid == 0) {    // 子进程
+        close(p1[1]);           // 关闭子进程p1写端口
+        read(p1[0], n1, sizeof(n1));    // p1管道中无数据时，阻塞
         printf("%d: received %s\n", getpid(), n1);
         write(p2[1], "pong", 5);
         close(p2[1]);
@@ -26,8 +27,8 @@ int main(int argc, char *argv[]) {
     } else {
         write(p1[1], "ping", 5);
         close(p1[1]);
-        close(p2[1]);
-        read(p2[0], n2, sizeof(n2));
+        close(p2[1]);       // 关闭主进程p2写端口
+        read(p2[0], n2, sizeof(n2));    // p2管道中无数据时，阻塞
         printf("%d: received %s\n", getpid(), n2);
         exit();
     }
